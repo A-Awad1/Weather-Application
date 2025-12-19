c54c98512a425b0c92afede4f12f85d17ee432f3
A-Awad1 <eng.a.awad1@gmail.com>
2025-12-19 08:07:26 +0200

    add drop down open logic

--- src/components/AppHeader/DropDown/index.tsx
+++ src/components/AppHeader/DropDown/index.tsx
@@ -1,20 +1,36 @@
-import type { MouseEvent } from "react";
+import { useState, useRef, useEffect, Activity, type MouseEvent } from "react";
 import MenuBox from "./MenuBox";
 import "./index.scss";
 
 export default function HeaderMenu() {
-  function openMenu(e: MouseEvent<HTMLButtonElement>) {
+  const [showMenu, setShowMenu] = useState(false);
+  const dropDownRef = useRef<HTMLDivElement>(null);
+
+  function toggleMenu(e: MouseEvent<HTMLButtonElement>) {
     e.preventDefault();
+    setShowMenu((prev) => !prev);
   }
 
+  function clickOut(event: globalThis.MouseEvent) {
+    const isOut = !dropDownRef?.current?.contains(event.target as Node);
+    if (isOut) setShowMenu(false);
+  }
+
+  useEffect(() => {
+    if (showMenu) document.addEventListener("mousedown", clickOut);
+    return () => document.removeEventListener("mousedown", clickOut);
+  }, [showMenu]);
+
   return (
-    <div className="drop-down">
-      <button className="head" onClick={openMenu}>
+    <div className="drop-down" ref={dropDownRef}>
+      <button className="head" onClick={toggleMenu}>
         <img className="gear-icon" src="/images/icon-units.svg" alt="settings icon" />
         <span>units</span>
         <img className="arrow-icon" src="/images/icon-dropdown.svg" alt="down arrow" />
       </button>
-      <MenuBox />
+      <Activity mode={showMenu ? "visible" : "hidden"}>
+        <MenuBox />
+      </Activity>
     </div>
   );
 }
