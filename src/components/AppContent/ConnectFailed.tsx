8793d780298996fc4eb0370a5be85d407cf3e237
A-Awad1 <eng.a.awad1@gmail.com>
2025-12-24 13:53:59 +0200

    update project stracture

--- src/components/AppContent/ConnectFailed.tsx
+++ src/components/AppContent/ConnectFailed.tsx
@@ -1,4 +1,4 @@
-import "./index.scss";
+import "./ConnectFailed.scss";
 
 export default function ConnectFailed() {
   return (
--- src/components/AppContent/index.tsx
+++ src/components/AppContent/index.tsx
@@ -1,8 +1,25 @@
 import ConnectFailed from "./ConnectFailed";
-import ConnectSuccess from "./ConnectSuccess";
+import SearchBar from "./SearchBar";
+import CurrentInfo from "./CurrentInfo";
+import DailyForecast from "./DailyForecast";
+import HourlyForecast from "./HourlyForecast";
+import "./index.scss";
 
 export default function AppContent() {
   const success = true;
-  if (success) return <ConnectSuccess />;
-  return <ConnectFailed />;
+  if (!success) return <ConnectFailed />;
+
+  return (
+    <section className="app-content">
+      <h2>How's the sky looking today?</h2>
+      <SearchBar />
+      <div className="result-info">
+        <div>
+          <CurrentInfo />
+          <DailyForecast />
+        </div>
+        <HourlyForecast />
+      </div>
+    </section>
+  );
 }
