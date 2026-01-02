a4498ab6124d907d5866a12777c70b6d74ed4dcb
A-Awad1 <eng.a.awad1@gmail.com>
2026-01-02 09:54:34 +0200

    update daily forecast  skeleton

--- src/components/AppContent/DailyForecast.tsx
+++ src/components/AppContent/DailyForecast.tsx
@@ -5,6 +5,25 @@ import { useSelector } from "react-redux";
 import type { RootState } from "~/store";
 import { getDayName } from "~/utils/methods";
 
+function Skeleton() {
+  const boxes = Array.from({ length: 7 }, () => null);
+  return (
+    <>
+      {boxes.map(() => (
+        <div>
+          <section className="skeleton"></section>
+          <h4>-</h4>
+          <img src="/general-icons/icon-loading.svg" alt="loading Icon" />
+          <div>
+            <span>-</span>
+            <span>-</span>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
 export default function DailyForecast() {
   const { loading, daily: data } = useSelector((state: RootState) => state.mainData);
 
@@ -12,17 +31,20 @@ export default function DailyForecast() {
     <article className="daily-forecast">
       <h3>Daily forecast</h3>
       <div>
-        {data?.time?.map((e, index) => (
-          <div key={e}>
-            {loading && <section className="skeleton"></section>}
-            <h4>{getDayName(e, "short")}</h4>
-            <ModeIcon weatherCode={data?.weatherCode[index]} />
-            <div>
-              <ConditionShow sort="temp" value={data?.maxTemp[index]} />
-              <ConditionShow sort="temp" value={data?.minTemp[index]} />
+        {loading ? (
+          <Skeleton />
+        ) : (
+          data?.time?.map((e, index) => (
+            <div key={e}>
+              <h4>{getDayName(e, "short")}</h4>
+              <ModeIcon weatherCode={data?.weatherCode[index]} />
+              <div>
+                <ConditionShow sort="temp" value={data?.maxTemp[index]} />
+                <ConditionShow sort="temp" value={data?.minTemp[index]} />
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </article>
   );
