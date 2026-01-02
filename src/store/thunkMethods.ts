50fbdc9a4e2a17d41439c2a4607c15ab2260d854
A-Awad1 <eng.a.awad1@gmail.com>
2026-01-02 08:58:29 +0200

    small enhancement

--- src/store/thunkMethods.ts
+++ src/store/thunkMethods.ts
@@ -15,16 +15,14 @@ const baseURL = "https://api.open-meteo.com/v1/forecast",
   daily = ["weather_code", "temperature_2m_max", "temperature_2m_min"].join(","),
   hourly = ["temperature_2m", "weather_code"].join(",");
 
+const delay = (period: number) => new Promise((resolve) => setTimeout(resolve, period));
+
 export const getMainData = createAsyncThunk(
   "mainData/getMainData",
   async ({ lat, lng }: { lat: number; lng: number }) => {
     const api = `${baseURL}?latitude=${lat}&longitude=${lng}&timezone=${timezone}&forecast_days=${forecast_days}&forecast_hours=${forecast_hours}&current=${current}&daily=${daily}&hourly=${hourly}`;
 
-    const [data] = await Promise.all([
-      fetch(api).then((res) => res.json()),
-      new Promise((resolve) => setTimeout(resolve, 500)),
-    ]);
-
+    const [data] = await Promise.all([fetch(api).then((res) => res.json()), delay(1000)]);
     return data;
   }
 );
@@ -34,11 +32,7 @@ export const getHourlyData = createAsyncThunk(
   async ({ lat, lng, date }: { lat: number; lng: number; date: string }) => {
     const api = `${baseURL}?latitude=${lat}&longitude=${lng}&timezone=${timezone}&hourly=${hourly}&start_date=${date}&end_date=${date}`;
 
-    const [data] = await Promise.all([
-      fetch(api).then((res) => res.json()),
-      new Promise((resolve) => setTimeout(resolve, 500)),
-    ]);
-
+    const [data] = await Promise.all([fetch(api).then((res) => res.json()), delay(500)]);
     return data;
   }
 );
